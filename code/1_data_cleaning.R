5d4e174c64ae14f8e5ef49c36d47f7f4e7cb39ce
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2026-02-14 21:21:40 +0000

    finish data cleaning script

--- code/1_data_cleaning.R
+++ code/1_data_cleaning.R
@@ -1,2 +1,29 @@
+######## INFO ########
+
+# PROJECT
+## Paper: YOUR PAPER TITLE
+## Authors: YOUR NAME
+
+# R Script
+## Purpose: This script performs data cleaning and generates the dataset for the formal model evaluation.
+## Inputs: data_raw/Brexit.csv
+## Outputs: data_clean/Brexit.rds
+
+# Setup ----
+
 library(tidyverse)
 library(here)
+
+i_am("code/1_data_cleaning.R") # helps with relative paths
+
+# Read in the data ----
+
+brexit_data <- read_csv(here("data_raw/Brexit.csv"))
+
+# Data cleaning ----
+
+# ... your data cleaning code here ...
+
+# Save the cleaned data ----
+
+write_rds(brexit_data, here("data_clean/Brexit.rds"))
