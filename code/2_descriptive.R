69a4c9906524070fa6c2743503e5b183ec6d2d8e
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2026-02-14 21:23:38 +0000

    start descriptive

---
+++ code/2_descriptive.R
@@ -0,0 +1,17 @@
+######## INFO ########
+
+# PROJECT
+## Paper: YOUR PAPER TITLE
+## Authors: YOUR NAME
+
+# R Script
+## Purpose: This script performs descriptive analysis and generates summary statistics for the dataset.
+## Inputs: data_clean/Brexit.rds
+## Outputs: 
+
+# Setup ----
+
+library(tidyverse)
+library(here)
+
+i_am("code/2_descriptive.R") # helps with relative paths
