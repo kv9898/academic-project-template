7aef758708cfaee4ad2b7c5333986a313be2c242
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2026-02-14 21:31:03 +0000

    Finish descriptive

--- code/2_descriptive.R
+++ code/2_descriptive.R
@@ -7,7 +7,7 @@
 # R Script
 ## Purpose: This script performs descriptive analysis and generates summary statistics for the dataset.
 ## Inputs: data_clean/Brexit.rds
-## Outputs: 
+## Outputs: outputs/turnout_by_region.rds
 
 # Setup ----
 
@@ -15,3 +15,20 @@ library(tidyverse)
 library(here)
 
 i_am("code/2_descriptive.R") # helps with relative paths
+
+# Read in the cleaned data ----
+
+brexit_data <- read_rds(here("data_clean/Brexit.rds"))
+
+# Descriptive plot ----
+
+(
+  turnout_by_region <- ggplot(brexit_data, aes(x= region, y = turnout)) +
+    geom_boxplot() +
+    labs(title = "Voter Turnout by Region",
+         x = "Region",
+         y = "Voter Turnout (%)") +
+    theme_minimal()
+)
+
+write_rds(turnout_by_region, here("outputs/turnout_by_region.rds"))
