d0f5e7903eaeb70a479c2af28b8ec1ee425cb4fc
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2026-05-09 16:24:50 +0100

    rename snapshots

--- code/2_descriptive.R
+++ code/2_descriptive.R
@@ -28,5 +28,5 @@ brexit_data <- read_rds(here("data_processed/Brexit.rds"))
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)))
 
-resultcheck::snapshot(turnout_by_region, "turnout_by_region.rds") # ensure the plot is consistent across runs
+resultcheck::snapshot(turnout_by_region, "turnout_by_region") # ensure the plot is consistent across runs
 write_rds(turnout_by_region, here("outputs/turnout_by_region.rds"))
